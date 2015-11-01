81cfd23c9a17d7cd95cf69c74c6770c89eabdd2c
Andre <andre.andrade@embedded.ufcg.edu.br>
2015-11-01 02:09:04 -0300

    Closes issue #3 - Working for text filds. Examples to show how it works and building form from template file

--- src/gen-form.directive.js
+++ src/gen-form.directive.js
@@ -7,8 +7,8 @@ angular.module('GenForm').
 		  	  template: '=template',
 		      model: '=model'
 		    },
-		    templateUrl: '/src/gen-form.html',
-		    //template: '<div ng-switch="child.type"> <div ng-switch-when="Object" ng-init="model[child.name]={}"> <h4 style="margin-left:{{indent}}px">{{child.name}}</h4> <div ng-repeat="ch in child.children"> <gen-form child="ch" model="model[child.name]" indent="indent+20"></gen-form> </div> </div> <div ng-switch-default> <div class="form-group" style="margin-left:{{indent}}px"> <label for="{{child.name}}">{{child.name}}</label> <input class="form-control" type="text" id="{{child.name}}" ng-model="model[child.name]"></input> </div> </div> </div>',
+		    //templateUrl: '/src/gen-form.html',
+		    template: '<div ng-switch="template.type"> <div ng-switch-when="Object" ng-init="model[template.name]={}"> <div ng-switch="template.name"> <div ng-switch-when="root"> <div ng-repeat="ch in template.children"> <gen-form template="ch" model="model[template.name]" indent="indent"></gen-form> </div> </div> <div ng-switch-default> <h4 style="margin-left:{{indent}}px">{{template.name}}</h4> <div ng-repeat="ch in template.children"> <gen-form template="ch" model="model[template.name]" indent="indent+20"></gen-form> </div> </div> </div> </div> <div ng-switch-default> <div class="form-group" style="margin-left:{{indent}}px"> <label for="{{template.name}}">{{template.name}}</label> <input class="form-control" type="text" id="{{template.name}}" ng-model="model[template.name]"></input> </div> </div> </div>',
 		    compile: function(element) {
 	        	return RecursionHelper.compile(element);
 	    	}
