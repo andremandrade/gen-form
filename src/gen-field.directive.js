46718a554f811d384bf91c46a730362dd425e5bd
Andre Meireles <andrema.pi@gmail.com>
2016-04-17 14:34:43 -0300

    issue #5 - Support to types: text, number, decimal, date, datetime, checkbox, enum, object. BUG: Example 2 does not works

--- src/gen-field.directive.js
+++ src/gen-field.directive.js
@@ -1,16 +1,14 @@
 angular.module('GenForm').
-	directive('genForm', function(RecursionHelper) {
+	directive('genField', function() {
 	 	return {
 		  	restrict: 'E',
 		  	scope: {
 		  	  indent: '=indent',
-		  	  template: '=template',
+		  	  field: '=field',
 		      model: '=model'
 		    },
-		    templateUrl: '/src/gen-form.html',
+		    templateUrl: '/src/gen-field.html',
 		    //template: '<div ng-switch="template.type"> <div ng-switch-when="Object" ng-init="model[template.name]={}"> <div ng-switch="template.name"> <div ng-switch-when="root"> <div ng-repeat="ch in template.children"> <gen-form template="ch" model="model[template.name]" indent="indent"></gen-form> </div> </div> <div ng-switch-default> <h4 style="margin-left:{{indent}}px">{{template.name}}</h4> <div ng-repeat="ch in template.children"> <gen-form template="ch" model="model[template.name]" indent="indent+20"></gen-form> </div> </div> </div> </div> <div ng-switch-default> <div class="form-group" style="margin-left:{{indent}}px"> <label for="{{template.name}}">{{template.name}}</label> <input class="form-control" type="text" id="{{template.name}}" ng-model="model[template.name]"></input> </div> </div> </div>',
-		    compile: function(element) {
-	        	return RecursionHelper.compile(element);
-	    	}
+		    
 	  	};
 	});
